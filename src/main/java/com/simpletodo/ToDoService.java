95e3174eea0d68ac7dfed7a6c63c984cd68a64d2
Mehdi Kharij <kharijmehdi38@gmail.com>
2024-10-08 00:30:22 +0100

    Ajout du service ToDo

---
+++ src/main/java/com/simpletodo/ToDoService.java
@@ -0,0 +1,10 @@
+package com.simpletodo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToDoService {
+    public void addTask(String task) {
+        // Logique pour ajouter une tâche
+    }
+}
