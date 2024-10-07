fbe3b4199000c1284b3b876927975caf049f75e9
Mehdi Kharij <kharijmehdi38@gmail.com>
2024-10-08 00:51:10 +0100

    2nd commit with conflicts

--- src/main/java/com/simpletodo/ToDoService.java
+++ src/main/java/com/simpletodo/ToDoService.java
@@ -6,5 +6,11 @@ import org.springframework.stereotype.Service;
 public class ToDoService {
     public void addTask(String task) {
         // Logique pour ajouter une tâche
+        System.out.println("Ajout de la tâche : " + task);
+    }
+
+    public void removeTask(String task) {
+        // Logique pour supprimer une tâche
+        System.out.println("Suppression de la tâche : " + task);
     }
 }
